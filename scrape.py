bb040e3e71c6b4939ee61c525f2a2fb3f3f2d2fe
emrecncelik <emrecncelik@gmail.com>
2024-09-13 16:49:56 +0300

    make download easier

--- scrape.py
+++ scrape.py
@@ -20,6 +20,12 @@ reddit = praw.Reddit(
 )
 
 
+@st.cache_data
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv(index=False).encode("utf-8")
+
+
 # Streamlit app
 def main():
     st.title("Reddit Comments Scraper")
@@ -38,33 +44,37 @@ def main():
             "parent_id",
             "is_top_level",
         ]
-        submission.comments.replace_more(limit=None)
-        for comment in submission.comments.list():
-            created_utc = datetime.datetime.fromtimestamp(
-                comment.created_utc, datetime.timezone.utc
-            )
-            is_top_level = int(comment.parent_id == comment.link_id)
-            comments.append(
-                (
-                    comment.body,
-                    comment.score,
-                    comment.author,
-                    created_utc,
-                    comment.id,
-                    comment.parent_id,
-                    is_top_level,
+        with st.spinner("Scraping comments..."):
+            submission.comments.replace_more(limit=None)
+            for comment in submission.comments.list():
+                created_utc = datetime.datetime.fromtimestamp(
+                    comment.created_utc, datetime.timezone.utc
                 )
+                is_top_level = int(comment.parent_id == comment.link_id)
+                comments.append(
+                    (
+                        comment.body,
+                        comment.score,
+                        comment.author,
+                        created_utc,
+                        comment.id,
+                        comment.parent_id,
+                        is_top_level,
+                    )
+                )
+
+            # Create a DataFrame
+            df = pd.DataFrame(comments, columns=columns)
+            st.write(df)
+
+            # Download CSV
+            csv = convert_df(df)
+            st.download_button(
+                label="Download data as CSV",
+                data=csv,
+                file_name="comments.csv",
+                mime="text/csv",
             )
-
-        # Create a DataFrame
-        df = pd.DataFrame(comments, columns=columns)
-        st.write(df)
-
-        # Download CSV
-        csv = df.to_csv(index=False)
-        b64 = base64.b64encode(csv.encode()).decode()
-        href = f'<a href="data:file/csv;base64,{b64}" download="comments.csv">Download CSV File</a>'
-        st.markdown(href, unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
